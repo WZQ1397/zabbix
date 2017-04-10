6753621e122070b9e494b8a248a760e3602edef5
WZQ1397 <wzqsergeant@vip.qq.com>
2017-04-10 16:45:22 +0800

    DISK IO MON

---
+++ template/DISK_IO/check_harddisk.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+diskname_discovery () {
+    HardDisk=($(grep '\b[a-z][a-z][a-z]\+\b'  /proc/diskstats|awk '{print $3}'))
+    [ "${HardDisk[0]}" == "" ] && exit
+    printf '{\n'
+    printf '\t"data":[\n'
+    for((i=0;i<${#HardDisk[@]};++i))
+    {
+        num=$(echo $((${#HardDisk[@]}-1)))
+        if [ "$i" != ${num} ];
+        then
+           printf "\t\t{ \n"
+           printf "\t\t\t\"{#DISKNAME}\":\"${HardDisk[$i]}\"},\n"
+        else
+           printf  "\t\t{ \n"
+           printf  "\t\t\t\"{#DISKNAME}\":\"${HardDisk[$num]}\"}]}\n"
+        fi
+    }
+}
+
+case "$1" in
+diskname_discovery)
+    diskname_discovery
+    ;;
+*)
+    echo "Usage: $0 {diskname_discovery}"
+    ;;
+esac
